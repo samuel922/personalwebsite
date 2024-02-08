d91e79d9595088cf15feed7a2bedf330b8825caf
samuel922 <onyangosamuel583@gmail.com>
2024-02-08 09:41:44 +0300

    Configured a middleware

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact page</title>
+</head>
+<body>
+    <h1>Contact Page</h1>
+    <form action="">
+        <label for="">Name</label>
+        <input type="text">
+
+        <button type="submit">Submit</button>
+    </form>
+    <a href="{{ route('about.page') }}">About</a>
+</body>
+</html>
